8c800765af22d44327fc22a9e791757665d2e3bb
Ssmunna <ss.munna2025@gmail.com>
2025-09-20 13:20:06 +0600

    [Add] app setting done

---
+++ app/Http/Middleware/TrackVisitor.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\Visitor;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class TrackVisitor
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $ip = $request->ip();
+
+        // Store only if new visitor
+        Visitor::firstOrCreate(['ip' => $ip]);
+
+        return $next($request);
+    }
+}
