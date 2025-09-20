c49cbb467c74537e7ecb52c9d9d53a71433bb196
Ssmunna <ss.munna2025@gmail.com>
2025-09-20 14:31:09 +0600

    [Add] app setting done

--- routes/web/frontend/home.php
+++ routes/web/frontend/home.php
@@ -4,3 +4,7 @@ use App\Http\Controllers\Frontend\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/',[HomeController::class,'HomePage'])->name('home');
+
+//Route::get('/contact', function() {
+//    return view('frontend.contact');
+//});
